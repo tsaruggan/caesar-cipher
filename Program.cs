ed6d7534b4374e5e32d1aff4d79ef9cc52bbfa9f
tsaruggan <tsaruggan@gmail.com>
2020-01-02 00:15:19 -0500

    initial commit for caesar cipher

---
+++ Program.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace caesar_cipher
+{
+    class Program
+    {
+        public static String[] alphabet = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
+        public static String[] shiftedAlphabet = new String[26];
+        const String wordListFileName = "words.txt";
+
+        private static void shift(int key)
+        {
+            // if key shifts alphabet one or more times, simplify to shift less than 26 char
+            if (key > 26 || key < -26) { key = key % 26; }
+
+            // if key is negative, simplify to positive
+            if (key < 0) { key = 26 + key; }
+
+            int current = key; // current is index of the starting char wanted for new alphabet
+            for (int i = 0; i < alphabet.Length; i++)
+            {
+                shiftedAlphabet[i] = alphabet[current]; // set characters for new alphabet
+                current++;
+                if (current > 25) { current = 0; }// if end of alphabet is reached, reset index back to 'A'
+            }
+        }
+
+        public static String encrypt(String message, int key)
+        {
+            String encryptedMsg = ""; // initialize encrypted message
+            message = Regex.Replace(message, "[^a-zA-Z]", "").ToUpper();  // remove all non letter characters from string +  make upper case
+            shift(key); // applies shift to alphabet by given key
+
+            // for each letter in message, swap with corresponding letter in new alphabet
+            for (int x = 0; x < (message.Length); x++)
+            {
+                int index = 0; // index value of original alphabet
+                bool check = false;
+
+                // loops until the index of the letter in original alpha is found for single letter in message
+                while (!check)
+                {
+                    String currentLetter = message[x].ToString(); // hold current letter of message
+                    if (string.Compare(currentLetter, alphabet[index]) == 0)
+                    { // compare if letter in message matches letter in alpha of current index
+                        check = true; // if found, break loop
+                        encryptedMsg = encryptedMsg + shiftedAlphabet[index]; // add the letter from the corresponding index of new alpha
+                    }
+                    else
+                    {
+                        index++; // if not found, add to index and check again
+                    }
+                }
+            }
+            return encryptedMsg;
+        }
+
+        public static String decrypt(String message, int key)
+        {
+            String decryptedMsg = ""; // initialize decrypted message
+            message = Regex.Replace(message, "[^a-zA-Z]", "").ToUpper();  // remove all non letter characters from string +  make upper case
+            shift(key); // applies shift to alphabet by given key
+
+            // for each letter in encrypted message, swap with corresponding letter in original alpha
+            for (int x = 0; x < (message.Length); x++)
+            {
+                int index = 0; // index value of new alphabet
+                bool check = false;
+
+                // loops until the index of the letter in new alpha is found for single letter in encrypted message
+                while (check == false)
+                {
+                    String currentLetter = message[x].ToString(); // hold current letter of encrypted message
+                    if (string.Compare(currentLetter, shiftedAlphabet[index]) == 0)
+                    {
+                        check = true; // if found, break loop
+                        decryptedMsg = decryptedMsg + alphabet[index]; // add the letter from corresponding index of original alpha
+                    }
+                    else
+                    {
+                        index++; // if not found, add to index and check again
+                    }
+                }
+            }
+            return decryptedMsg;
+        }
+
+        public static String hack(String encryptedMsg)
+        {
+            String hackedMsg = ""; // initialize hacked message
+            int topMatches = 0; // the most words found within a decrypted message
+
+            // for each possible message, check the number of real words exist it contains and compare
+            for (int key = 0; key < 26; key++)
+            {
+                int matches = 0; // number of match points for current decrypted message
+                try
+                {
+                    System.IO.StreamReader file = new System.IO.StreamReader(Path.Combine(Environment.CurrentDirectory, wordListFileName));
+                    String word; // read from file of over 58 000 english words
+
+                    // loop for every word in .txt file
+                    while ((word = file.ReadLine()) != null)
+                    {
+                        // check if decrypted msg contains word
+                        if (decrypt(encryptedMsg, key).Contains(word)) { matches += word.Length; } // if word exists, award match points depending on length of matching word  
+                    }
+
+                    if (matches > topMatches)
+                    { // if word has most match points, becomes the most probable message
+                        topMatches = matches;
+                        hackedMsg = decrypt(encryptedMsg, key); // set hacked message to word with top matche points
+                    }
+
+                }
+                catch (System.IO.FileNotFoundException)
+                {
+                }
+            }
+            return hackedMsg;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WELCOME TO CAESAR CIPHER\n\nEnter 1 for encrypt...\nEnter 2 for decrypt...\nEnter 3 for hack...\nEnter 4 for testing...\n\n");
+            int input;
+            try
+            {
+                input = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();
+
+                switch (input)
+                {
+                    case 1:
+                        Console.WriteLine("Input message to encrypt: ");
+                        String message = Console.ReadLine();
+                        Console.Clear();
+                        // ensures message contains at least a single english letter
+                        if (Regex.IsMatch(message, ".*[a-zA-Z]+.*") == true)
+                        {
+                            // prompts user for key
+                            Console.WriteLine("Input key: ");
+                            int keyE = 0;
+
+                            try
+                            { // checks if key is a valid integer
+                                keyE = Convert.ToInt32(Console.ReadLine());
+                                Console.Clear();
+                            }
+                            catch (Exception e)
+                            { // if key is invalid, make key equal 0 and show error message
+                                Console.WriteLine("Error. key = 0");
+                                keyE = 0;
+                            }
+
+                            // perform encryption and display on console
+                            String encrypted = encrypt(message, keyE);
+                            Console.WriteLine("Encrypted message: " + encrypted);
+                        }
+                        else
+                        { // if message contains no real letters, display error message
+                            Console.WriteLine("Error. Must enter letters from the alphabet.\n\nRestart");
+                        }
+                        break;
+
+                    case 2:
+                        Console.WriteLine("Input message to decrypt: ");
+                        String userInputEncryptedMsg = Console.ReadLine();
+                        Console.Clear();
+                        if (Regex.IsMatch(userInputEncryptedMsg, ".*[a-zA-Z]+.*") == true)
+                        {
+                            // prompts user for key
+                            Console.WriteLine("Input key: ");
+                            int keyD = 0;
+
+                            try
+                            { // checks if key is a valid integer
+                                keyD = Convert.ToInt32(Console.ReadLine());
+                                Console.Clear();
+                            }
+                            catch (Exception e)
+                            { // if key is invalid, make key equal 0 and show error message
+                                Console.WriteLine("Error. key = 0");
+                                keyD = 0;
+                            }
+
+                            // perform decryption and display on console
+                            String decryptedMsg = decrypt(userInputEncryptedMsg, keyD);
+                            Console.WriteLine("Decrypted message:" + decryptedMsg);
+
+                        }
+                        else
+                        { // if userInputEncryptedMsg contains no real letters, display error message
+                            Console.WriteLine("Error. Invalid encrypted message.\n\nRestart");
+                        }
+                        break;
+                    case 3:
+                        // prompt for encrypted message
+                        Console.WriteLine("Input encrypted message: ");
+                        String encryptedMsg = Console.ReadLine();
+                        Console.Clear();
+                        Console.WriteLine("please wait one moment... \n\n"); // display wait message as hacking may take tim
+                        String hackedMsg = hack(encryptedMsg); // call for hacking function
+                        Console.WriteLine("Hacked message: " + hackedMsg); // output hacked message
+                        break;
+                    case 4:
+                        //prompt for testing
+                        Console.WriteLine("Enter file directory of testing messages (.txt):  ");
+                        String inputFilename = Console.ReadLine();
+                        Console.WriteLine("Enter filename to save test results as (.xlsx):  ");
+                        String outputFilename = Console.ReadLine();
+                        Testing.test(inputFilename, outputFilename);
+                        break;
+                    default:
+                        throw new Exception("Not a valid input. Must enter 1, 2, 3 or 4. Please restart.");
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.Clear();
+                Console.WriteLine("There was an error handling your input. Please restart.");
+            }
+        }
+    }
+}
---
+++ Testing.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using OfficeOpenXml;
+
+namespace caesar_cipher
+{
+    class Testing
+    {
+        public static void test(String inputFilename, String outputFilename)
+        {
+            List<String> testMessagesList = loadTestMessages(inputFilename); //store cleaned messages from given text file (.txt)
+            String[,] results = new String[testMessagesList.Count,3]; // create multidimensional string array to hold results
+
+            Random random = new Random();
+            Stopwatch stopwatch = new Stopwatch();
+
+            Console.Clear();
+            Console.WriteLine("Please wait until testing is complete...");
+
+            for (int index  = 0; index < testMessagesList.Count; index++) //for each message 
+            {
+                String message = testMessagesList[index]; //store current message 
+                String length = message.Length.ToString(); //store length of current message
+
+                int key = random.Next(1, 27); //generate random key
+                String encrypted = Program.encrypt(message, key); //encrypt current message using key
+
+                stopwatch.Start(); //start stopwatch
+                String hacked = Program.hack(encrypted); //hack encrypted message
+                stopwatch.Stop(); //stop stopwatch
+
+                String time = Math.Round(stopwatch.Elapsed.TotalSeconds, 2).ToString(); //store time in seconds
+                stopwatch.Reset(); //reset timer
+
+                String success = (message == hacked).ToString(); //check if hack was succesful and store boolean
+
+                results[index, 0] = length;
+                results[index, 1] = time;
+                results[index, 2] = success;
+            }
+            exportToExcel(results, outputFilename); //export results to excel
+            Console.WriteLine($"Testing is complete. Results saved as: {outputFilename}");
+        }
+
+
+        private static List<String> loadTestMessages(String filename)
+        {
+            List<String> messages = new List<String>(); //string list to hold each cleaned message
+            try
+            {
+                //store text file from given directory relative to bin directory of project
+                System.IO.StreamReader file = new System.IO.StreamReader(Path.Combine(Environment.CurrentDirectory, filename));
+                String word;
+
+                // loop for every word in .txt file
+                while ((word = file.ReadLine()) != null)  //while still lines in text file, hold each line
+                {
+                    word = Regex.Replace(word, "[^a-zA-Z]", "").ToUpper();  // remove all non letter characters from string +  make upper case
+                    messages.Add(word); //store word in string list
+                }
+            }
+            catch (System.IO.FileNotFoundException) //catch if reading file directory does not work
+            {
+                Console.WriteLine("Error. Input file directory does not exist or is not the correct type (.txt).\n\nRestart");
+            }
+            return messages; //return string list of cleaned messages
+        }
+
+        private static void exportToExcel(String [,] results,  String filename)
+        {
+            ExcelPackage excel = new ExcelPackage();
+            ExcelWorksheet worksheet = excel.Workbook.Worksheets.Add("Worksheet1"); //create worksheet
+            FileInfo excelFile = new FileInfo(filename); 
+
+            DataTable dataTable = new DataTable(); //create datatable
+           
+            //create excel table headers
+            dataTable.Columns.Add("Length", typeof(int));
+            dataTable.Columns.Add("Time to Hack (s)", typeof(double));
+            dataTable.Columns.Add("Success", typeof(Boolean));
+
+            for (int index = 0; index < results.GetLength(0); index++) //store data from test results in each row
+            {
+                dataTable.Rows.Add(results[index, 0], results[index, 1], results[index, 2]);
+            }
+
+            worksheet.Cells["A1"].LoadFromDataTable(dataTable, true); //add table to worksheet
+
+            for (int index = 0; index < results.GetLength(0); index++)
+            {
+                //set fill type of first 3 cells in data rows to solid fill
+                worksheet.Cells[$"A{index + 2}:C{index + 2}"].Style.Fill.PatternType = OfficeOpenXml.Style.ExcelFillStyle.Solid;
+
+                if (Convert.ToBoolean(results[index, 2])) //check if hack was successful
+                {       
+                    worksheet.Cells[$"A{index+2}:C{index+2}"].Style.Fill.BackgroundColor.SetColor(Color.Green); //if true fill green
+                } else
+                {
+                    worksheet.Cells[$"A{index + 2}:C{index + 2}"].Style.Fill.BackgroundColor.SetColor(Color.Red); //if false fill red
+                }
+            }
+
+            worksheet.Cells[worksheet.Dimension.Address].AutoFitColumns(); //autofit each column
+            excel.SaveAs(excelFile); //save excel file with given filename
+        }
+    }
+}
